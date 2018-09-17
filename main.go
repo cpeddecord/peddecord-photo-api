9d5351b60f75006f04d1a11f75d39b150ca12a90
Chris Peddecord <cpeddecord@gmail.com>
2018-09-17 19:55:00 -0400

    now with a json response

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+type response struct {
+	Hash   string `json:hash`
+	Date   string `json:date`
+	Secret string `json:secret`
+}
+
 var commitHash = os.Getenv("COMMIT_REF")
 var buildDate = os.Getenv("BUILD_DATE")
+var secret = os.Getenv("TEST_SECRET")
 
 // look at these sweet comments
 func main() {
+	res := response{
+		commitHash,
+		buildDate,
+		secret,
+	}
+
+	data, _ := json.Marshal(res)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "seeing if these pipes truly work\n\nbuild %s at %s", commitHash, buildDate)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
 	})
 
 	fmt.Println("Build Hash: ", commitHash)
